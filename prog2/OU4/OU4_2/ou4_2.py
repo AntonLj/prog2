bc6539d1261c12a5e39508977c64f709843302a3
Anton Ljungar <anlj5496@siegbahn.it.uu.se>
2021-06-07 01:10:10 +0200

    testar igen då

--- prog2/OU4/OU4_2/ou4_2.py
+++ prog2/OU4/OU4_2/ou4_2.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python3
 
 from heltal import Heltal
+import matplotlib.pyplot as plt
+from time import perf_counter as pc
+
+def fib_py(n):
+	if n<=1:
+		return n
+	else:
+		return (fib_py(n-1)+fib_py(n-2))
 
 def main():
-	f = Heltal(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
+	pytime = []
+	ctime = []
+	for i in range(30,35):
+		t1 = pc()
+		f=Heltal(i)
+		f.fib()
+		t2 = pc()
+		fib_py(i)
+		t3 = pc()
+		pytime.append(t3-t2)
+		ctime.append(t2-t1)
+	n = [*range(30,35)]
+	fig, ax = plt.subplots()
+	ax.plot(n,pytime)
+	ax.set(xlabel='n',ylabel='seconds (s)',title='C++ vs Python (fib)')
+	ax.grid()
+	fig.savefig('test.png')
+	plt.show()
+	f = Heltal(40)
+	print(f'Time with n = 47: {f.fib()}')
 
 if __name__ == '__main__':
 	main()
